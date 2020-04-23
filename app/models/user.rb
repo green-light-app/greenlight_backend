bc0bd9fb6c7303238ef88fd0ea883937e5a0c243
pirainogi <pirainogi@gmail.com>
2020-04-23 15:24:50 -0400

    renamed matchee to crush

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,17 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :matches
-  has_many :inverse_matches, foreign_key: :matchee_id, class_name: Match
+  has_many :inverse_matches, foreign_key: :crush_id, class_name: Match
   has_one :info
 
   def all_matches
-   matches_array = matches.map{|match| match.matchee if match.confirmed}
-   matches_array + inverse_matches.map{|match| match.user if match.confirmed}
-   matches_array.compact
- end
+    matches_array = matches.map{|match| match.crush if match.confirmed}
+    matches_array + inverse_matches.map{|match| match.user if match.confirmed}
+    matches_array.compact
+  end
 
   def pending_matches
-    matches.map{|match| match.matchee if !match.confirmed}.compact
+    matches.map{|match| match.crush if !match.confirmed}.compact
   end
 
   def friend_requests
--- db/migrate/20200423181832_create_matches.rb
+++ db/migrate/20200423181832_create_matches.rb
@@ -2,10 +2,10 @@ class CreateMatches < ActiveRecord::Migration[5.2]
   def change
     create_table :matches do |t|
       t.references user, index: true, foreign_key: true
-      t.references matchee, index: true
+      t.references crush, index: true
       t.boolean confirmed
       t.timestamps
     end
-    add_foreign_key :matches, :users, column: :matchee_id
+    add_foreign_key :matches, :users, column: :crush_id
   end
 end
